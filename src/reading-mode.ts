04877605d5a5a8ab17696a576f18893d76f92613
Lila Rest <mail@lila.rest>
2023-01-26 13:44:14 +0100

    refactor: makes a cleaner file hierarchy by isolating code by usage

---
+++ src/reading-mode.ts
@@ -0,0 +1,56 @@
+
+export function readingModeRenderer(element: any, context: any) {
+
+    // Retrieve the blocks' container element 
+    // @ts-ignore
+    const blocksContainer = context.containerEl;
+    
+    // Listen for insertion of the element into the blocks' container
+    const observer = new MutationObserver(() => {
+
+        // If the element has been inserted
+        if (blocksContainer.contains(element)) {
+
+            // And if the current block is a custom class block
+            const classBlock = [...element.querySelectorAll('code')].find((codeEl) => codeEl.innerText.trim().startsWith(CUSTOM_CLASS_ANCHOR));
+            if (classBlock) {
+
+                // Retrieve the custom class name
+                const nextBlockClass = classBlock.innerText.trim().replace(CUSTOM_CLASS_ANCHOR, "").trim();
+
+                // Set the 'data-next-block-class' attribute with the custom class name
+                element.setAttribute("data-next-block-class", nextBlockClass);
+                
+                // Remove the classBlock element from the render
+                element.innerHTML = "";
+            }
+            
+            // Then ensure that all classes are properly applied in the context
+            let nextBlockClass = null;
+            for (const block of blocksContainer.children) {
+                // Reset the block classes
+                block.className = "";
+                    
+                // If the block is a custom class block
+                if (block.getAttribute("data-next-block-class")) {
+
+                    // Set the next block class
+                    nextBlockClass = block.getAttribute("data-next-block-class");
+                }
+
+                // Else if the block is preceded by a custom class block
+                else if (nextBlockClass) {
+                    // Add the custom class
+                    block.classList.add(nextBlockClass)
+
+                    // Reset nextBlockClass
+                    nextBlockClass = null;
+                }
+            }
+
+            // Finally, stop listening for element insertion.
+            observer.disconnect();
+        }
+    });
+    observer.observe(blocksContainer, { attributes: false, childList: true, characterData: false, subtree: true });
+}
---
+++ src/settings.ts
@@ -0,0 +1,1 @@
+const CUSTOM_CLASS_ANCHOR = 'class:';
