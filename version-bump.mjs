23614f023b44a3e63e6b61ff296f24778a67ea5e
Lila Rest <mail@lila.rest>
2023-01-29 17:35:04 +0100

    feat: fake commit to test release

--- version-bump.mjs
+++ version-bump.mjs
@@ -7,10 +7,7 @@ const writeJSON = (filename, obj) => {
     writeFileSync(filename, JSON.stringify(obj, null, '\t'));
 };
 
-const targetVersion = process.env.npm_package_version;
-
 const manifest = readJSON('manifest.json');
-writeJSON('manifest.json', { ...manifest, version: targetVersion });
 
 const versions = readJSON('versions.json');
-writeJSON('versions.json', { ...versions, [targetVersion]: manifest.minAppVersion });
+writeJSON('versions.json', { ...versions, [manifest.version]: manifest.minAppVersion });
