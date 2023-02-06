5d51816c3cb4510e10b9d863da9c37bebda7641f
Lila Rest <mail@lila.rest>
2023-02-06 18:12:45 +0100

    fix: try to fix CI that doesn't include bumped files + fake commit to trigger

--- version-bump.mjs
+++ version-bump.mjs
@@ -1,4 +1,6 @@
 import { readFileSync, writeFileSync } from "fs";
+import { version } from "os";
+console.log("BUUUUUUUUUMP");
 
 let packageJSON = JSON.parse(readFileSync("package.json", "utf8"));
 
@@ -7,8 +9,10 @@ let manifest = JSON.parse(readFileSync("manifest.json", "utf8"));
 const { minAppVersion } = manifest;
 manifest.version = packageJSON.version;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
+console.log(manifest);
 
 // update versions.json with target version and minAppVersion from manifest.json
 let versions = JSON.parse(readFileSync("versions.json", "utf8"));
 versions[packageJSON.version] = minAppVersion;
-writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));                      
+writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
+console.log(versions);                    
